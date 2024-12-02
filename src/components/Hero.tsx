8225e5e25b36ea822d418f85312bde2d01d3dfef
unknown <you@example.com>
2024-12-02 12:34:47 -0800

    first commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="text-gray-600 body-font bg-fixed bg-cover bg-center bg-image ">
+    <section className="text-gray-600 body-font bg-fixed bg-cover bg-center bg-image">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
@@ -27,7 +27,7 @@ export default function Hero() {
             tomorrow&quot;.
           </p>
           <div className="flex justify-center">
-            <Link href={"#contact"}>
+            <Link href="#contact">
               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                 Contact
               </button>
@@ -38,7 +38,7 @@ export default function Hero() {
           <Image
             className="object-cover object-center rounded mx-auto w-[15rem]"
             src="/images/mypic.jpg"
-            alt="Hero"
+            alt="Muhammad Arsalan, Web Developer"
             width={500}
             height={500}
           />
